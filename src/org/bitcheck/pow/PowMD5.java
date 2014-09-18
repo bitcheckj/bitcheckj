bd7da4285cdf9215f07f8e02d88d7e32a04dfc6e
Alex Shvid <a@shvid.com>
2014-09-18 15:45:42 -0700

    mode bits in PowMD5

--- src/org/bitcheck/pow/PowMD5.java
+++ src/org/bitcheck/pow/PowMD5.java
@@ -9,7 +9,7 @@ public class PowMD5 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Pow");
+		System.out.println("PowMD5");
 		
 		Random r = new Random();
 		
@@ -18,29 +18,29 @@ public class PowMD5 {
 		
 		long time0 = System.currentTimeMillis();
 		
-		int mode = 500;
+		int mode = 35;
 		
 		//int x = 400;//r.nextInt(500);
 		//int y = x;
 		
 		for (long i = 1; i != Long.MAX_VALUE; ++i) {
 			
-			x = calc(x);
+			x = calc(x, mode);
 			
 
-			y = calc(y);
-			y = calc(y);
+			y = calc(y, mode);
+			y = calc(y, mode);
 
 			if (Arrays.equals(x, y)) {
 
-				System.out.println("step = " + i + ", x = " + x);
+				System.out.println("step = " + i + ", x = " + Arrays.toString(x));
 				
 				long meetStep = i;
 				byte[] meetValue = x;
 				
 				for (; i != Long.MAX_VALUE; ++i) {
 
-					x = calc(x);
+					x = calc(x, mode);
 
 					
 					if (Arrays.equals(x, meetValue)) {
@@ -62,9 +62,7 @@ public class PowMD5 {
 		System.out.println("timeMls = " + (time1 - time0));
 		
 	}
-	
-	private static int a = 345634446;
-	private static int b = 124151255;
+
 	
 	private static MessageDigest md;
 	
@@ -76,12 +74,55 @@ public class PowMD5 {
 	}
 	}
 	
-	public static byte[] calc(byte[] x) {
+	public static byte[] calc(byte[] x, int mode) {
+		
+		if (mode >= 16*8) {
+			throw new IllegalArgumentException("invalid mode " + mode);
+		}
 		
 		md.reset();
 		byte[] result = md.digest(x);
 		
-		Arrays.fill(result, 0, 9, (byte)0);
+		int bits = mode % 8;
+		int fullBytes = mode / 8;
+		int lastZeroIndex = 16 - fullBytes;
+		if (bits > 0) {
+			lastZeroIndex--;
+		}
+		
+		Arrays.fill(result, 0, lastZeroIndex, (byte)0);
+
+		if (bits > 0) {
+			
+			int b = result[lastZeroIndex];
+			
+			switch(bits) {
+			case 7:
+				b &= 0x7F;
+				break;
+			case 6:
+				b &= 0x3F;
+				break;
+			case 5:
+				b &= 0x1F;
+				break;
+			case 4:
+				b &= 0x0F;
+				break;
+			case 3:
+				b &= 0x07;
+				break;
+			case 2:
+				b &= 0x03;
+				break;
+			case 1:
+				b &= 0x01;
+				break;
+			}
+			
+			result[lastZeroIndex] = (byte) b;
+			
+		}
 		
 		return result;
 	}
